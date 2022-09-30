75e062710159b6f8a2a2ef9000fb064cf896d36c
mys1024 <mys.1024@outlook.com>
2022-10-01 02:15:45 +0800

    refactor: optimize

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,36 @@
 import type { Plugin } from 'vite'
-import type { VitePluginWatOptions } from './types'
 
 import fse from 'fs-extra'
-import { resolve, relative, isAbsolute } from 'pathe'
+import { resolve, relative } from 'pathe'
 import { wat2wasm } from './wabt'
 
-const watIdSuffixReg = /\.wat\?init$/
+const WAT_ID_REG = /\.wat\?init$/
+const CWD = resolve(process.cwd())
+const WASM_DIR = resolve(CWD, 'node_modules/.vite-plugin-wat/wasm')
 
 function isWatId(id: string): boolean {
-  return watIdSuffixReg.test(id)
+  return WAT_ID_REG.test(id)
 }
 
-function toWasmId(watId: string, cwd: string, wasmDir: string) {
-  return resolve(wasmDir, relative(cwd, watId).replace(watIdSuffixReg, '.wasm'))
+function resolveWasmId(watId: string) {
+  return resolve(WASM_DIR, relative(CWD, watId).replace(WAT_ID_REG, '.wasm'))
 }
 
-export default (options?: VitePluginWatOptions): Plugin => {
-  // default options
-  const {
-    wasmDir = 'node_modules/.vite-plugin-wat/wasm'
-  } = options || {}
-
-  // paths
-  const cwd = resolve(process.cwd())
-  const resolvedWasmDir = isAbsolute(wasmDir)
-    ? wasmDir
-    : resolve(cwd, wasmDir)
-
-  // plugin
+export default (): Plugin => {
   return {
     name: 'vite-plugin-wat',
-
     config: () => ({
-      server: {
-        watch: {
-          ignored: [resolvedWasmDir]
-        }
-      }
+      server: { watch: { ignored: [WASM_DIR] } }
     }),
-
-    resolveId(id) {
+    resolveId: (id) => {
       if (!isWatId(id))
         return
       return id
     },
-
-    async transform(code, id) {
+    transform: async (code, id) => {
       if (!isWatId(id))
         return
-      const wasmId = toWasmId(id, cwd, resolvedWasmDir)
+      const wasmId = resolveWasmId(id)
       await fse.ensureFile(wasmId)
       await fse.writeFile(wasmId, await wat2wasm(code))
       return `import initWasm from '${wasmId}?init';\nexport default initWasm;`
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface VitePluginWatOptions {
-  /**
-   * Output directory of the generated WASM files. (default: 'node_modules/.vite-plugin-wat/wasm')
-   */
-  wasmDir?: string
-}
