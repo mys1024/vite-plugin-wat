17095a1dc6fac135ce7742fd6bcda0ceaa768a79
mys1024 <mys.1024@outlook.com>
2022-10-01 01:58:26 +0800

    chore: update example

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,31 +1,25 @@
-import './style.css'
 import initAddModule from './add.wat?init'
+import './style.css'
 
 interface AddModuleExports {
   add(a: number, b: number): number
   addAndMul(a: number, b: number, c: number): number
 }
 
-const addModuleInstance = await initAddModule({})
-const addModuleExports = addModuleInstance.exports as unknown as AddModuleExports
+// import from WASM module
+const addModuleExports = (await initAddModule({}))
+  .exports as unknown as AddModuleExports // type casting
 const { add } = addModuleExports
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <div class="center">
-      <button id="counter" type="button"></button>
-    </div>
+  <div class="center">
+    <button id="counter" type="button">count is 0</button>
   </div>
 `
 
-function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
-  const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(add(counter, 1)))
-  setCounter(0)
-}
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const Counter = document.querySelector<HTMLButtonElement>('#counter')
+let cnt = 0
+Counter!.addEventListener('click', () => {
+  cnt = add(cnt, 1)
+  Counter!.innerHTML = `count is ${cnt}`
+})
