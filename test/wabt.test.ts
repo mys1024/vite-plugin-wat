4b0644a2e2d47781979004324bb18268c4d81b62
mys1024 <mys.1024@outlook.com>
2022-10-02 04:00:23 +0800

    test: add wat2wasm correctness test

--- test/wabt.test.ts
+++ test/wabt.test.ts
@@ -1,18 +1,31 @@
 import { describe, expect, it } from 'vitest'
 import { wat2wasm } from '../src/wabt'
 
-describe.concurrent('base64 encoding', () => {
-  it('wat2wasm', async () => {
-    const wasm = await wat2wasm(`
-      (module
-        (func $add (param i32) (param i32) (result i32)
-          local.get 0
-          local.get 1
-          i32.add
-        )
-        (export "add" (func $add))
+describe('wat2wasm', () => {
+  const wat = `
+    (module
+      (func $add (param i32) (param i32) (result i32)
+        local.get 0
+        local.get 1
+        i32.add
       )
-    `)
+      (export "add" (func $add))
+    )
+  `
+
+  it('compiling', async () => {
+    const wasm = await wat2wasm(wat)
     expect(wasm).toMatchSnapshot()
+  })
+
+  it('correctness', async () => {
+    const wasm = await wat2wasm(wat)
+    const { add } = (
+      await (WebAssembly.instantiate(wasm, {}))
+    ).instance.exports
+    if (add instanceof Function)
+      expect(add(1, 2)).toBe(3)
+    else
+      throw new Error('\'add\' is not a function')
   })
 })
