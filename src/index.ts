8054d3deed09e37bea4fc68d951bf379e1ebcee4
mys1024 <mys.1024@outlook.com>
2022-10-02 16:05:51 +0800

    refactor: use cache in transforming

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { relative, resolve } from 'pathe'
 import fse from 'fs-extra'
 
 import { wat2wasm } from './wabt'
+import { TimingMap } from './util'
 
+const PLUGIN_NAME = 'vite-plugin-wat'
 const WAT_ID_REG = /\.wat\?init$/
 
 function isWatId(id: string): boolean {
@@ -16,10 +18,15 @@ function resolveWasmId(watId: string, cwd: string, wasmDir: string) {
 
 export default (): Plugin => {
   const cwd = resolve('./')
-  const storeDir = resolve(cwd, 'node_modules', '.vite-plugin-wat')
+  const storeDir = resolve(cwd, 'node_modules', `.${PLUGIN_NAME}`)
   const wasmDir = resolve(storeDir, 'wasm')
+  const transformCache = new TimingMap<
+    string,
+    { code: string; output: string }
+  >()
+
   return {
-    name: 'vite-plugin-wat',
+    name: PLUGIN_NAME,
     config: () => ({
       server: { watch: { ignored: [wasmDir] } },
     }),
@@ -32,10 +39,19 @@ export default (): Plugin => {
     transform: async (code, id) => {
       if (!isWatId(id))
         return
+      // check cache
+      const cache = transformCache.get(id)
+      if (cache?.code === code)
+        return cache.output
+      // wasmId will be a string like '/path/to/store/wasm/generated.wasm'
       const wasmId = resolveWasmId(id, cwd, wasmDir)
+      // compile `.wat` to `.wasm`, save `.wasm` in `wasmDir`
       await fse.ensureFile(wasmId)
       await fse.writeFile(wasmId, await wat2wasm(code))
-      return `export { default } from '${wasmId}?init';`
+      // transform to export from '*.wasm?init'
+      const output = `export { default } from '${wasmId}?init';`
+      transformCache.set(id, { code, output }, 1000 * 60 * 10) // cache will expire after 10 min
+      return output
     },
   }
 }
